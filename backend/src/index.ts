c223d42c62b7033c6334c9c5eff5d7a8401a2737
buitai97 <taibui97@outlook.com>
2024-12-03 22:24:25 -0800

    rm node_modules

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+    console.log("Connected to the database!");
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+}
+
+connectToDatabase();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/my/user", myUserRoute);
+
+app.listen(3000, () => {
+  console.log("server started on http://localhost:3000");
+});
